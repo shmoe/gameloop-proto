e409c91276ecad78dcaa37b9fd227f6d9530e43b
Griffin Nelson <griffinnelson@outlook.com>
2020-05-24 16:33:56 -0400

    added Scene class to hold Actors added an empty scene to main()

---
+++ include/Scene.hpp
@@ -0,0 +1,39 @@
+#ifndef __SCENE_HPP__
+#define __SCENE_HPP__
+
+#include <SFML/Graphics.hpp>
+#include <set>
+#include "Actor.hpp"
+
+/**
+ * holds all of the Actors in the current world
+ * as well as the current viewport
+*/
+class Scene {
+	private:
+		sf::RenderWindow* window;				//pointer to the RenderWindow
+		sf::Transform viewport_transform;		//offset of the viewport from world origin
+		sf::Vector2f scale_factor;				//scale factor for the sprites relative to the viewport size
+		std::set<Actor*> actors;				//set of actors, sorted by z-axis postion
+	public:
+		Scene(sf::RenderWindow*);				//default constructor
+
+		void setScaleFactor(sf::Vector2f);		//set current viewport scale factor
+
+		void addActor(Actor*);					//add an actor to the scene
+		void removeActor(Actor*);				//remove an actor from the scene
+		void clear();							//remove all actors from scene
+
+		std::set<Actor*> getActors();			//get all actors in the scene
+		std::set<Actor*> getVisibleActors();	//get all actors within the viewport
+
+		void update();							//update all actors in the scene by one step of the physics engine
+		void updateVisible();					//update all actors within the viewport by one step of the physics engine
+		void updateVisible(float tolerance);	//update all actors within viewport with a tolerance of *tolerance*
+
+		void render();							//render the part of the scene visible from the viewport
+		void render(float steps_ahead);			//render the part of the scene visible from the viewport, 
+													//interpolated *steps_ahead* physics engine steps in the future
+};
+
+#endif//__SCENE_HPP__
