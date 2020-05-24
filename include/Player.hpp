94c5793e7d46362c73a998d7d571c351b929e606
Griffin Nelson <griffinnelson@outlook.com>
2020-05-24 16:12:46 -0400

    made Player extend Actor added new states to Player State

--- include/Player.hpp
+++ include/Player.hpp
@@ -2,9 +2,8 @@
 #define __PLAYER_HPP__
 
 #include <vector>
-#include <SFML/Graphics.hpp>
+#include "Actor.hpp"
 
-#include "Vector.hpp"
 enum PlayerState{
 	STANDING,
 	RUNNING,
@@ -17,30 +16,19 @@ enum PlayerState{
 	RAMBI_ATTACKING,
 	ENGUARDE_SWIMMING,
 	ENGUARDE_ATTACKING,
-	EXPRESSO_FALLING
+	EXPRESSO_FALLING,
+	MINECART_MOVING,
+	MINECART_FALLING
 };
-class Player {
+class Player : public Actor {
 	private:
-		sf::Transform worldTransform;						//Player character's current place in the world, relative to the scene origin
-		sf::Texture* spriteSheet;							//A pointer to the sprite sheet, which lives in the ResourceManager
-		sf::Sprite current_sprite;							//holds the current frame of animation
+		PlayerState state;									//the player character's current state
 	public:
 		void setState(PlayerState state);					//set the player character's current state
 		PlayerState getState();								//get the player character's current state
 
-		sf::Transform getTransform();						//get the transform to be done to the sprite, relative to the scene origin
-		sf::Transform getTransform(float steps_ahead);		//get the transform to be done to the sprite, relative to the scene origin,
-																//interpolated *steps_ahead* physics engine steps in the future
-		sf::Drawable* getCurrentSprite(float steps_ahead);	//get the current sprite in the animation, interpolated *floor(steps_ahead)*,
-																//physics engine steps in the future
-
-		void update(std::vector<bool> keys);				//update the player character by one step of the physics engine
-
-		//expose internels for collisions and other external interactions
-		void transform(sf::Transform transform);			//transform the player character, relative to it's current position in the scene
-		Vector getVelocity();								//return the current velocity of the player character as a polar vector
-		Vector getAcceleration();							//return the current velocity of the player character as a polar vector
-		float getRotationalVelocity();						//probably not relevant
-		float getRotationalAcceleration();					//probably not relevant
+		void update();										//update the player character by one step of the physics engine (do not use)
+		void update(std::vector<bool> keys);				//update the player character by one step of the physics engine and the sprite by one
+																//frame of animation
 };
 #endif//__PLAYER_HPP__
